f0f8207f2219841b9581bb2118289cba5b0b7723
rpf3 <rpf3@protonmail.ch>
2015-05-31 18:08:31 -0400

    Use jade layout engine

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+/// <reference path="typings/node/node.d.ts"/>
+
 var express = require( 'express' );
+var path = require( 'path' );
+
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.get( '/', function( req, res ) {
-    res.send( 'Hello World!' );
+    res.render( 'index' );
 });
 
 var server = app.listen( 3000 );
