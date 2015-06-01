ecea298c77cb552d5280d183556f696679b1787f
rpf3 <rpf3@protonmail.ch>
2015-05-31 21:03:58 -0400

    Use bootstrap cover framework

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var path = require( 'path' );
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'view engine', 'jade' );
+
+app.use( express.static( path.join( __dirname, 'styles' ) ) );
 
 app.get( '/', function( req, res ) {
     res.render( 'index' );
