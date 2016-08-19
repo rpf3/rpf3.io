92fb2c06291beffb5ab957541f0271efc592c8bc
rpf3 <rpf3@protonmail.ch>
2016-08-19 08:28:52 -0400

    Install the Ghost blogging platform.

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 /// <reference path="typings/node/node.d.ts"/>
 
-var express = require( 'express' );
-var path = require( 'path' );
+var express = require('express');
+var ghost = require('ghost');
+var path = require('path');
 
 var app = express();
 
-// view engine setup
-app.set( 'views', path.join( __dirname, 'views' ) );
-app.set( 'view engine', 'jade' );
+// Set up the jade view engine.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use( express.static( path.join( __dirname, 'styles' ) ) );
+app.use(express.static(path.join( __dirname, 'styles')));
 
-app.get( '/', function( req, res ) {
-    res.render( 'index' );
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
-var server = app.listen( 3000 );
+// Start Ghost.
+ghost().then(function(ghostServer) {
+    // Mount Ghost to the configured subdirectory.
+    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+
+    // Start Ghost using the parent express app.
+    ghostServer.start(app);
+});
+
+var server = app.listen(3000);
